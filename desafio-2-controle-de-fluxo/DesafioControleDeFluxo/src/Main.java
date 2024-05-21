a0928513a1abc4c5f1b0622dd966ec81c825672a
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2024-05-21 10:09:02 -0300

    Run Contador on main and throws exception on error

--- desafio-2-controle-de-fluxo/DesafioControleDeFluxo/src/Main.java
+++ desafio-2-controle-de-fluxo/DesafioControleDeFluxo/src/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Main working");
+  public static void main(String[] args) throws ParametrosInvalidosException {
+    Scanner terminal = new Scanner(System.in);
+    System.out.println("Digite o primeiro parâmetro");
+    int parametroUm = terminal.nextInt();
+    System.out.println("Digite o segundo parâmetro");
+    int parametroDois = terminal.nextInt();
+
+    try {
+      //chamando o método contendo a lógica de contagem
+      Contador.contar(parametroUm, parametroDois);
+
+    } catch (ParametrosInvalidosException exception) {
+      throw new RuntimeException(exception);
+    }
+
   }
 }
